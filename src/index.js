60271299be19a6d730f8d1bde93407d7a8d4a6fc
Viktoriia <pecourses>
2023-11-05 17:28:02 +0200

    Add store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,53 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { legacy_createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+// початковий стан
+const initialState = {
+  count: 0,
+  step: 1,
+};
+
+// reducer - чиста ф-я (state, action) => state
+// action - {type: 'increment', payload} - js-object з обов'язковою властивістю type
+function counterReducer(state = initialState, action) {
+  const { type } = action;
+  switch (type) {
+    case 'increment': {
+      const { count, step } = state;
+      return { ...state, count: count + step };
+    }
+
+    case 'decrement': {
+      const { count, step } = state;
+      return { ...state, count: count - step };
+    }
+    case 'setStep': {
+      const { value } = action;
+      return { ...state, step: value };
+    }
+
+    default:
+      return state;
+  }
+}
+
+// сховище = стан + як змінювати стан (редюсер)
+const store = legacy_createStore(
+  counterReducer,
+  composeWithDevTools(applyMiddleware())
+);
+
+// Provider можливий дає доступ до стори з компонентів
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
